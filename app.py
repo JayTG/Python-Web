eb9616c798a80f877cedc57f2bdfabc0189a9004
dt18jjt <87706066+JayTG@users.noreply.github.com>
2021-07-21 14:07:53 +0100

    html page templates added and  blog post for loop
    template for pages in HTML have been added. fake blog posts have been added to the home page through a for loop taken from a list in the app script

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-#Home
+
+#blog post content list
+posts = [
+    {
+        'author' : 'Dan',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'April 20 2020'
+    },
+    {
+        'author' : 'Dave',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'April 21 2020'
+    } 
+]
+
+#Home page
 @app.route("/")
+@app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
-#About
+    return render_template('home.html', posts=posts)
+
+#About page
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html')
 
-
+# if run on python go to debug mode
 if __name__ == '__main__':
     app.run(debug=True)
